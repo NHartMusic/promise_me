545bc790e4b4465ed64a82b0c6aea3b593011ea6
Nick Hart <nhartmusic@gmail.com>
2020-05-08 22:33:32 +0200

    promise you will be gentle

--- sketch.js
+++ sketch.js
@@ -1,11 +1,22 @@
-let giphyAPI = 'https://api.giphy.com/v1/gifs/random?api_key=40UYsq6LhA7BqcqLHRwB0xO7AOSd6P8J&tag=&rating=G'
-
 function setup() {
-
-    fetch(giphyAPI)
-        .then(data => console.log(data))
-        .then(json => console.log(json))
-        .catch(err => console.log(err))
+    delay(2000)
+        .then(() => alert('are you there god its me, 6ix9ine'))
+        .catch((err) => console.log(err))
 }
+
+function delay(time) {
+    return new Promise((resolve, reject) => {
+        if (isNaN(time)) {
+            reject(new Error('that is not a number'))
+        }
+
+        setTimeout(resolve, time)
+    })
+}
+
+function sayHello() {
+    alert('Rat juice')
+}
+
 
 setup()
