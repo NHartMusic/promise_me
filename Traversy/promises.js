ad46154d6f7e29e3309148d3b73f5dc97409127d
Nick Hart <nhartmusic@gmail.com>
2020-05-10 13:09:56 +0200

    more promise.all practice

--- Traversy/promises.js
+++ Traversy/promises.js
@@ -29,5 +29,16 @@ function createPost(post) {
     })
 }
 
-createPost({ title: 'Post Three', body: 'Invesitage 311' }).then(getPosts)
+// createPost({ title: 'Post Three', body: 'Invesitage 311' })
+//     .then(getPosts)
+//     .catch(err => console.log(err))
+
+//promise.all time!!!! 
+
+const promise1 = Promise.resolve('Hello world')
+const promise2 = 10
+const promise3 = new Promise((resolve, reject) => setTimeout(resolve, 2000, 'Bye Bye'))
+const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
+
+Promise.all([promise1, promise2, promise3, promise4]).then(((values) => console.log(values)))
 
